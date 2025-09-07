9e27d9cd2d89197be15a78549152a37cb90bac33
gaykun1 <139235847+gaykun1@users.noreply.github.com>
2025-09-07 10:48:53 +0300

    Add ping request to wake up python server

--- frontend/src/components/loads/AuthClientUpload.tsx
+++ frontend/src/components/loads/AuthClientUpload.tsx
@@ -5,6 +5,7 @@ import { getProfile } from '@/redux/authSlice';
 import { getMeasurement } from '@/redux/measurementSlice';
 import { getNutritionDay } from '@/redux/nutritionDaySlice';
 import { getWorkouts } from '@/redux/workoutsSlice';
+import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -15,16 +16,20 @@ const AuthClientUpload = () => {
     const router = useRouter();
     useEffect(() => {
         const getUser = async () => {
-           
+
             try {
                 const res1 = await api.get(`/api/auth/profile`);
-
                 dispatch(getProfile(res1.data.user));
-
-
             } catch {
                 router.push("/auth/login");
             }
+            // ping python server to wake up
+
+            await axios.get(`${process.env.NEXT_PUBLIC_PYTHON_API_URL}/api/ping`, {
+                withCredentials: true,
+                headers: { "Content-Type": "application/json" }
+            });
+
             const res4 = await api.get("api/measurement/measurements");
             dispatch(getMeasurement(res4.data));
             const res2 = await api.get(`/api/fitness-plan/workouts`);
