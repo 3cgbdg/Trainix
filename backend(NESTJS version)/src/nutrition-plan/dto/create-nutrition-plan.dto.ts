4d9e7bfd26fad5ffd9a0828c54e478a1e00297a3
gaykun1 <139235847+gaykun1@users.noreply.github.com>
2026-01-24 19:17:52 +0200

    feat: nutrition-plan service

--- backend(NESTJS version)/src/nutrition-plan/dto/create-nutrition-plan.dto.ts
+++ backend(NESTJS version)/src/nutrition-plan/dto/create-nutrition-plan.dto.ts
@@ -1,1 +1,46 @@
-export class CreateNutritionPlanDto {}
+import { Type } from "class-transformer";
+import { IsArray, ValidateNested, IsString, IsInt, IsOptional, IsDate, IsNotEmpty } from "class-validator";
+
+export class CreateNutritionPlanDto {
+    @ValidateNested({ each: true })
+    @Type(() => NutritionDayDto)
+    day: NutritionDayDto; 
+
+}
+
+
+ class NutritionDayDto {
+  @IsInt()
+  dayNumber: number; 
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  date?: Date; 
+
+  @ValidateNested({ each: true })
+  @Type(() => MealDto)
+  meals: MealDto[]; 
+
+  @IsOptional()
+  waterCurrent?: number;
+
+  @IsOptional()
+  waterTarget?: number;
+
+
+}
+
+class MealDto {
+  @IsString()
+  @IsNotEmpty()
+  mealTitle: string;
+
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
+}
+
+
+// other nested dtos type classes
+
--- backend(NESTJS version)/src/nutrition-plan/nutrition-plan.service.ts
+++ backend(NESTJS version)/src/nutrition-plan/nutrition-plan.service.ts
@@ -1,26 +1,179 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateNutritionPlanDto } from './dto/create-nutrition-plan.dto';
-import { UpdateNutritionPlanDto } from './dto/update-nutrition-plan.dto';
-
+import { PrismaService } from 'prisma/prisma.service';
+import {  ReturnNutritionDayType, ReturnNutritionStatisticsType } from 'src/types/nutrition-plan';
+import { IReturnMessage } from 'src/types/common';
 @Injectable()
 export class NutritionPlanService {
-  create(createNutritionPlanDto: CreateNutritionPlanDto) {
-    return 'This action adds a new nutritionPlan';
+  constructor(private readonly prisma: PrismaService) { };
+
+  async createNutritionPlan(dto: CreateNutritionPlanDto, myId: string) {
+    const dayDate = new Date();
+    const date = dayDate.setDate(dayDate.getDate() + dto.day.dayNumber - 1);
+    console.log(dayDate);
+
+    // if plan exists then we pushing new day otherwise creating new plan including the day in it 
+    let nutritionPlan = await this.prisma.nutritionPlan.findUnique({ where: { userId: myId } });
+
+    // parallel for optimized using in adding images to each meal
+    await Promise.all(
+      dto.day.meals.map(async (meal) => {
+        const image = await this.prisma.mealImage.findUnique({ where: { name: meal.mealTitle } });
+        if (!image) {
+
+
+          const url = await s3ImageUploadingMeal(meal);
+
+          await this.prisma.mealImage.create(
+            {
+              data: {
+                name: meal.mealTitle,
+                url
+              }
+            },
+          );
+          meal.imageUrl = url;
+        }
+
+      }))
+
+    // if plan exists just pushing
+    if (nutritionPlan) {
+      nutritionPlan.days.push(obj);
+      await nutritionPlan.save();
+      res.status(200).json({ message: "Successfully added day!" });
+      return;
+    }
+    // otherwise creating plan with this item
+    else {
+      nutritionPlan = await NutritionPlan.create({ userId: (req as AuthRequest).userId, "days": [obj], createdAt: new Date() });
+      res.status(201).json({ message: "Nutrition plan created!", day: obj });
+      return;
+    }
   }
 
-  findAll() {
-    return `This action returns all nutritionPlan`;
+  async getNutritionDay(myId: string): Promise<ReturnNutritionDayType> {
+
+    const MS_PER_DAY = 1000 * 60 * 60 * 24;
+    const nutritionPlan = await this.prisma.nutritionPlan.findFirst({ where: { userId: myId } });
+    if (!nutritionPlan) {
+      throw new NotFoundException("Nutrition Plan was not found")
+    }
+
+    const dayNumber = Math.round(
+      ((new Date().getTime() - new Date(nutritionPlan.createdAt).getTime()) /
+        MS_PER_DAY) + 1
+    );
+
+    const nutritionDay = await this.prisma.nutritionDay.findUnique({ where: {nutritionPlanId_dayNumber:{ nutritionPlanId: nutritionPlan.id, dayNumber }}, include: { meals: { include: { ingredients: true, mealImage: true } }, dailyGoals: true, waterIntake: true } })
+
+    if (!nutritionDay) {
+      throw new NotFoundException("Nutrition Day was not found")
+    }
+
+
+    return nutritionDay;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} nutritionPlan`;
+  async updateWaterCurrent(myId: string, day: string, amount: number): Promise<IReturnMessage> {
+
+    if (amount <= 0) {
+      throw new BadRequestException("Amount must be positive");
+    }
+
+    // getting parsed to num idx of the day
+    const dayNum = Number(day);
+    const nutritionPlan = await this.prisma.nutritionPlan.findFirst({ where: { userId: myId } });
+    if (!nutritionPlan) {
+      throw new NotFoundException("Nutrition Plan was not found")
+    }
+
+    // adding numbers to current waterIntake
+    await this.prisma.nutritionDay.update({ where: { nutritionPlanId_dayNumber:{dayNumber: dayNum, nutritionPlanId: nutritionPlan.id} }, data: { waterCurrent: { increment: amount } } })
+
+
+
+    return ({ message: "Status updated!" })
   }
 
-  update(id: number, updateNutritionPlanDto: UpdateNutritionPlanDto) {
-    return `This action updates a #${id} nutritionPlan`;
+  async getWeekStatistics(myId: string, week: string): Promise<ReturnNutritionStatisticsType> {
+
+    const weekNumber = Number(week);
+
+    // finding week number forlater loop idx using
+    const nutritionPlan = await this.prisma.nutritionPlan.findFirst({ where: { userId: myId } });
+    if (!nutritionPlan) {
+      throw new NotFoundException("Nutrition Plan was not found")
+    }
+
+    const days = await this.prisma.nutritionDay.findMany({ where: {nutritionPlanId: nutritionPlan.id }, include: { dailyGoals: true }, orderBy: { dayNumber: "asc" }, take: 7, skip: 7 * (weekNumber - 1) });
+
+    if (days.length == 0) {
+      throw new NotFoundException("Days were not found");
+    }
+    // loop for 7 days
+
+    let data = days.map(day => (
+      {
+        day: day.date.toLocaleDateString("en-US", { weekday: "short" }),
+        calories: day.dailyGoals?.caloriesCurrent,
+        protein: day.dailyGoals?.proteinCurrent,
+        carbs: day.dailyGoals?.carbsCurrent,
+        fats: day.dailyGoals?.fatsCurrent
+
+      }
+    ))
+
+
+    return ({ data })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} nutritionPlan`;
+
+  async updateMealStatus(myId: string, day: string, mealId: string): Promise<IReturnMessage> {
+
+    const dayNum = Number(day);
+
+    const mealInfo = await this.prisma.meal.findUnique({ where: { id: mealId } });
+    if (!mealInfo) {
+      throw new NotFoundException("Meal was not found")
+
+    }
+
+    const nutritionPlan = await this.prisma.nutritionPlan.findFirst({ where: { userId: myId } });
+    if (!nutritionPlan) {
+      throw new NotFoundException("Nutrition Plan was not found")
+    }
+    //adding fresh numbers to dailyGoals - calories,fats,carbs,protein.
+    await this.prisma.nutritionDay.update({
+      where: { nutritionPlanId_dayNumber: { nutritionPlanId: nutritionPlan.id, dayNumber: dayNum } },
+      data: {
+
+        dailyGoals: {
+          update: {
+            fatsCurrent: { increment: mealInfo.mealFats },
+            carbsCurrent: { increment: mealInfo.mealCarbs },
+            caloriesCurrent: { increment: mealInfo.mealCalories },
+            proteinCurrent: { increment: mealInfo.mealProtein },
+          }
+        }
+        ,
+        meals: { update: { where: {id:mealId}, data: { status: "EATEN" } } }
+      }
+    })
+
+    return ({ message: "Status updated!" });
+
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
